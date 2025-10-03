51613e17b699148c757d03416b3150660c71f56f
dancingactor <s990346@gmail.com>
2025-10-04 02:05:08 +0800

    feat: add the main page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero.jsx";
+import HeroCard from "./components/HeroCard/HeroCard.jsx";
+import planetVideo from "./assets/GSFC_20190627_TESS_m13223_L98-59b_Full_Rotation~orig.mp4"
+import Rapidscat from "./components/Rapidscat/Rapidscat.jsx";
+import Satelite from "./components/Satelite/Satelite.jsx";
+import Footer5 from "./components/Footer/Footer5.jsx";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      duration: 1200,
+      easing: "ease-in-out",
+    });
+  });
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="">
+      <div className="h-[700px] relative">
+        <video
+          autoPlay
+          loop
+          muted
+          className="fixed right-0 top-0 h-[700px] w-full object-cover z-[-1]"
+        >
+          <source src={planetVideo} type="video/mp4" />
+        </video>
+        <Navbar />
+        <Hero />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <HeroCard />
+      <Rapidscat />
+      <Satelite />
+      <Footer5 />
+    </div>
+  );
+};
 
-export default App
+export default App;
